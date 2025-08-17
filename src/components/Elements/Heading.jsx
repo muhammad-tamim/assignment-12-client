76fc7480d46ece2513cd4eaf7b0d09c0e20723c7
tamim-111 <tamim144362@gmail.com>
2025-08-17 19:10:11 +0600

    style(contactInfo): impore ui for dark and white mode

--- src/components/Elements/Heading.jsx
+++ src/components/Elements/Heading.jsx
@@ -1,6 +1,6 @@
 const Heading = ({ title }) => {
     return (
-        <h1 className="text-5xl font-bold mb-12 text-center text-[#25A8D6]">
+        <h1 className="text-5xl font-bold mb-20  text-center text-[#25A8D6]">
             {title}
         </h1>
     )
