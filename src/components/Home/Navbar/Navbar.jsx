75f405f60230d5f42c436280ae201d37bb355b25
tamim-111 <tamim144362@gmail.com>
2025-08-17 10:51:11 +0600

    style(navbar): add light and dark  mode

--- src/components/Home/Navbar/Navbar.jsx
+++ src/components/Home/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { AiOutlineMenu } from 'react-icons/ai'
 import { FaGlobeAmericas, FaShoppingCart } from 'react-icons/fa'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { NavLink, Link } from 'react-router'
 import avatarImg from '../../../assets/images/placeholder.jpg'
 import logo from '../../../assets/images/logo.svg'
@@ -10,6 +10,18 @@ import useAuth from '../../../hooks/useAuth'
 const Navbar = () => {
     const { user, logOut } = useAuth()
     const [isOpen, setIsOpen] = useState(false)
+
+
+    const [theme, setTheme] = useState('light');
+
+    // Apply theme to <html>
+    useEffect(() => {
+        document.querySelector('html').setAttribute('data-theme', theme);
+    }, [theme]);
+
+    const handleThemeToggle = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    };
 
     return (
         <div className='fixed w-full bg-gradient-to-r from-[#6BDCF6] to-[#25A8D6] z-10 shadow-sm text-white'>
@@ -71,87 +83,93 @@ const Navbar = () => {
                         </div>
 
                         {/* Right Side: Profile Dropdown*/}
-                        <div className='relative'>
-                            <div
-                                onClick={() => setIsOpen(!isOpen)}
-                                className='p-2 border-[1px] border-neutral-200 flex items-center gap-2 rounded-full cursor-pointer hover:shadow-md transition'
-                            >
-                                <AiOutlineMenu className='text-white' />
-                                <img
-                                    className='rounded-full'
-                                    referrerPolicy='no-referrer'
-                                    src={user?.photoURL || avatarImg}
-                                    alt='profile'
-                                    height='30'
-                                    width='30'
-                                />
-                            </div>
+                        <div className='flex gap-4 items-center'>
+                            <button className="btn btn-ghost btn-circle " onClick={handleThemeToggle}>
+                                {theme === 'light' ? '🌙' : '☀️'}
+                            </button>
 
-                            {/* Dropdown Content */}
-                            {isOpen && (
-                                <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[12vw] bg-white overflow-hidden right-0 top-12 text-sm z-50'>
-                                    <div className='flex flex-col cursor-pointer text-black'>
-                                        <div className='md:hidden flex flex-col'>
-                                            <Link
-                                                to='/'
-                                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                            >
-                                                Home
-                                            </Link>
-                                            <Link
-                                                to='/shop'
-                                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                            >
-                                                Shop
-                                            </Link>
-                                            <Link
-                                                to='/cart'
-                                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                            >
-                                                Cart
-                                            </Link>
-                                            {!user && <Link
-                                                to='/login'
-                                                className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                            >
-                                                Join Us
-                                            </Link>}
-                                        </div>
-
-                                        {user ? (
-                                            <>
-                                                <Link
-                                                    to='/update-profile'
-                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                                >
-                                                    Update Profile
-                                                </Link>
-                                                <Link
-                                                    to='/dashboard'
-                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
-                                                >
-                                                    Dashboard
-                                                </Link>
-                                                <div
-                                                    onClick={() => {
-                                                        setIsOpen(false)
-                                                        logOut()
-                                                    }}
-                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
-                                                >
-                                                    Logout
-                                                </div>
-                                            </>
-                                        ) : (
-                                            <p
-                                                className='px-4 py-3 text-xs text-red-400'
-                                            >
-                                                Please Login to see below features
-                                            </p>
-                                        )}
-                                    </div>
+                            <div className='relative'>
+                                <div
+                                    onClick={() => setIsOpen(!isOpen)}
+                                    className='p-2 border-[1px] border-neutral-200 flex items-center gap-2 rounded-full cursor-pointer hover:shadow-md transition'
+                                >
+                                    <AiOutlineMenu className='text-white' />
+                                    <img
+                                        className='rounded-full'
+                                        referrerPolicy='no-referrer'
+                                        src={user?.photoURL || avatarImg}
+                                        alt='profile'
+                                        height='30'
+                                        width='30'
+                                    />
                                 </div>
-                            )}
+
+                                {/* Dropdown Content */}
+                                {isOpen && (
+                                    <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[12vw] bg-white overflow-hidden right-0 top-12 text-sm z-50'>
+                                        <div className='flex flex-col cursor-pointer text-black'>
+                                            <div className='md:hidden flex flex-col'>
+                                                <Link
+                                                    to='/'
+                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                >
+                                                    Home
+                                                </Link>
+                                                <Link
+                                                    to='/shop'
+                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                >
+                                                    Shop
+                                                </Link>
+                                                <Link
+                                                    to='/cart'
+                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                >
+                                                    Cart
+                                                </Link>
+                                                {!user && <Link
+                                                    to='/login'
+                                                    className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                >
+                                                    Join Us
+                                                </Link>}
+                                            </div>
+
+                                            {user ? (
+                                                <>
+                                                    <Link
+                                                        to='/update-profile'
+                                                        className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                    >
+                                                        Update Profile
+                                                    </Link>
+                                                    <Link
+                                                        to='/dashboard'
+                                                        className='px-4 py-3 hover:bg-neutral-100 transition font-semibold'
+                                                    >
+                                                        Dashboard
+                                                    </Link>
+                                                    <div
+                                                        onClick={() => {
+                                                            setIsOpen(false)
+                                                            logOut()
+                                                        }}
+                                                        className='px-4 py-3 hover:bg-neutral-100 transition font-semibold cursor-pointer'
+                                                    >
+                                                        Logout
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <p
+                                                    className='px-4 py-3 text-xs text-red-400'
+                                                >
+                                                    Please Login to see below features
+                                                </p>
+                                            )}
+                                        </div>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </Container>
